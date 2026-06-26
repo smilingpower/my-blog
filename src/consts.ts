ea428f3619110e02fc4464ab90665b07860d1d1a
smilingpower <email@gmail.com>
2026-06-26 16:00:38 +0800

    更新網站標題

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Sheena Li's Blog';
+export const SITE_DESCRIPTION = '個人品牌・作品集・學習筆記!';
